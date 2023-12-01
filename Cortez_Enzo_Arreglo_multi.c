616920b16ae90cedc01a3324b1edd9221759cb81
EnzoCortez <enzo.cortez@udla.edu.ec>
2023-11-30 21:30:45 -0500

    Inicializacion del programa y definicion de las matrices

---
+++ Cortez_Enzo_Arreglo_multi.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int m, n, l;
+
+    /* Solicitud de las dimensiones de las matrices */
+    
+    printf("Ingrese el numero de matrices: ");
+    scanf("%d", &l);
+
+    printf("Ingrese el numero de filas: ");
+    scanf("%d", &m);
+
+    printf("Ingrese el numero de columnas: ");
+    scanf("%d", &n);
